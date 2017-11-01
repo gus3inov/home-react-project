cd221ec37703b0eb72d3ef7d2ac9c5435963e8c2
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-02 00:16:54 +0300

    Start project from 0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Root from './components/Root'
 
-render(<Root />, document.getElementById('container'))
+function HelloWorld(){
+    return <h1>Helloooooo</h1>
+}
+
+render(<HelloWorld />, document.getElementById('container'))
