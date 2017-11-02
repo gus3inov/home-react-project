0028d0fbe52d49f2d5032a7d6c79ca3419cee674
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-02 21:03:48 +0300

    Render article component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import {render} from 'react-dom'
+import Article from './Article'
+import {articles} from './fixtures'
+
+const article = articles[0]
 
 function HelloWorld(){
-    return <h1>Helloooooo</h1>
+    return (
+        <div>
+        <Article article={article}/>
+        </div>
+    );
 }
-
 render(<HelloWorld />, document.getElementById('container'))
