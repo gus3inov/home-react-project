8ae91e8a4b0ae131267d553d8566ded808318150
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-02 22:00:25 +0300

    Render article with map

---
+++ src/ArticleList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Article from './Article'
+
+export default function ArticleList({ articles }){
+    const articleRender = articles.map(value => <li key={value.id}><Article article={value}/></li>)
+    return (
+        <ul>
+            {articleRender}
+        </ul>
+    )
+}
