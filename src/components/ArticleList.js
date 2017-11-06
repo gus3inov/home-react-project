ac40120b997cc0bd48f77ce5cc5ab95d5931fcb8
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-06 18:46:24 +0300

    Open only one article - "accordion"

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react'
 import Article from './Article'
 import PropTypes from 'prop-types'
 
-export default function ArticleList({ articles }){
-    const articleRender = articles.map(value => <li key={value.id}><Article article={value}/></li>)
+export default class ArticleList extends Component{
+    state = {
+        openArticleId: null
+    }
+    
+    render() {
+        const articleRender = this.props.articles.map(value => 
+        <li key={value.id}>
+            <Article 
+                article = { value }
+                isOpen = { value.id === this.state.openArticleId }
+                toggleOpen = { this.toggleOpenArticle.bind(this, value.id) }
+                />
+        </li>)
+
     return (
         <ul>
             {articleRender}
         </ul>
-    )
+      )
+    }
+
+    toggleOpenArticle = (openArticleId) => {
+        this.setState({
+            openArticleId
+        })
+    }
+
 }
 
 ArticleList.propTypes = {
