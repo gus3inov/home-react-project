d975a24156a6f0606f54f67f0d3cc613984fabcb
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-11 23:59:19 +0300

    Add select form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import UserForm from './UserForm.js'
+import UserForm from './UserForm'
 import ArticleList from './ArticleList'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
 
 
 export default class App extends Component {
@@ -9,12 +11,25 @@ export default class App extends Component {
         articles: PropTypes.array.isRequired
     };  
 
+    state = { 
+      selection: null
+    }
+
   render() {
+      const options = this.props.articles.map(artilce =>({
+        label: artilce.title,
+        value: artilce.id
+      }))
+
     return (
       <div>
           <UserForm />
+          <Select options = { options } value = { this.state.selection } onChange = { this.changeSelection } multi = { true } />
           <ArticleList articles = { this.props.articles }/>
       </div>
     )
   }
+
+  changeSelection = selection => this.setState({ selection })
+
 }
