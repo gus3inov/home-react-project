453b51e18b4a729b568536bc36b0e1f60f28607e
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-11 23:08:49 +0300

    Add UserForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import UserForm from './UserForm.js'
 import ArticleList from './ArticleList'
+
 
 export default class App extends Component {
     static propTypes = {
@@ -10,6 +12,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+          <UserForm />
           <ArticleList articles = { this.props.articles }/>
       </div>
     )
