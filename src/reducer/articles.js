901e8f0ac4ef4cbb5dfc839f2741470e6cd901fc
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-26 20:14:06 +0300

    use constance

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -5,7 +5,7 @@ export default (articlesState = defaultArticles, action) => {
     const { type, payload } = action
 
     switch(type){
-        case 'DELETE_ARTICLE': return articlesState.filter(article => article.id !== payload.id)
+        case DELETE_ARTICLE: return articlesState.filter(article => article.id !== payload.id)
     }
 
     return articlesState
