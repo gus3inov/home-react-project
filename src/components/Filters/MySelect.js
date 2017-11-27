386e89cd3f523113446c7070c502b2fb4004bbd0
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-27 18:41:00 +0300

    connect redux and react, state for selection

--- src/components/Filters/MySelect.js
+++ src/components/Filters/MySelect.js
@@ -2,27 +2,37 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
+import { selectFilter } from '../../AC'
+import { connect } from 'react-redux'
 
 
-export default class MySelect extends Component {
+class MySelect extends Component {
     static propTypes = {
         
     }
 
-    state = {
-        selection: null
+    changeSelection = () => {
+        const { deleteArticle, testScore } = this.props
+        console.log(testScore.map(article =>  article.value))
+        selectFilter(testScore.map(article =>  article.value))
     }
 
     render() {
-        const options = this.props.articles.map(artilce =>({
-            label: artilce.title,
-            value: artilce.id
-          }))
-
+          console.log(this.props.testScore)
         return (
         <div>
-            <Select options       = { options } value = { this.state.selection } onChange = { this.changeSelection } multi = { true } />
+            <Select options = { this.props.testScore } value = { null } onChange = { this.changeSelection } multi = { true } />
         </div>
         )
     }
 }
+
+export default connect(
+    state => ({
+        testScore: state.articles.map(artilce =>({
+            label: artilce.title,
+            value: artilce.id
+          }))
+    }),
+    { selectFilter }
+)(MySelect)
