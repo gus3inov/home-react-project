6a00ff4857b78c8595cec995fa1855f64503285f
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-28 21:55:24 +0300

    connect reduce for date filter

--- src/components/Filters/MyDatePicker.js
+++ src/components/Filters/MyDatePicker.js
@@ -5,68 +5,33 @@ import 'react-day-picker/lib/style.css'
 import { DateUtils } from "react-day-picker"
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
+import { dateFilter } from '../../AC'
 
 
 class MyDatePicker extends Component {
 
-    state = { 
-      from: null,
-      to: null
-    }
+  handleDayClick = (day) => {
+    const { dateFilter, range } = this.props
+    dateFilter(DateUtils.addDayToRange(day, range))
+}
 
-    handleDayClick = (day) => {
-      const range = DateUtils.addDayToRange(day, this.state);
-      this.setState(range);
-    }
-
-    handleResetClick = e => {
-      e.preventDefault();
-      this.setState({
-        from: null,
-        to: null,
-      });
-    }
-
-  render() {
-      const { from, to } = this.state
-      const fromDate = new Date(from)
-      const toDate = new Date(to)
-  
-    return (
-      <div>
-          <DayPicker
-          numberOfMonths={1}
-          selectedDays={[from, { from, to }]}
-          onDayClick={this.handleDayClick}
-          fixedWeeks
-        />
-        <div>
-        {!from && !to && <p>Please select the <strong>first day</strong>.</p>}
-        {from && !to && <p>Please select the <strong>last day</strong>.</p>}
-        {from &&
-          to &&
-          <p className = "date-result">
-            You chose from 
-            {' '}
-            <span className="red">
-            {  <Moment format="DD/MM/YYYY" date={fromDate} /> }
-            </span>
-            {' '}
-            to
-            {' '}
-            <span className="red">
-           {   <Moment format="DD/MM/YYYY" date={toDate} />  }
-           </span>
-          .
-            {' '}<a href="." onClick={this.handleResetClick}>Reset</a>
-          </p>}
+    render() {
+      const { from, to } = this.props.range;
+      const selectedRange = from && to && `${from.toDateString()} - ${to.toDateString()}`
+      return (
+          <div className="date-range">
+              <DayPicker
+                  ref="daypicker"
+                  selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                  onDayClick={ this.handleDayClick }
+              />
+              {selectedRange}
           </div>
-          </div>
-    )
+      );
   }
-
-  changeSelection = selection => this.setState({ selection })
 
 }
 
-export default MyDatePicker
+export default connect(state => ({
+  range: state.filters.dateRange
+}), { dateFilter })(MyDatePicker)
