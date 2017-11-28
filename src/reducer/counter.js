72703c463ed55041b847224e71364f24a92e44e4
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-28 21:57:22 +0300

    modiefed decrement of counter

--- src/reducer/counter.js
+++ src/reducer/counter.js
@@ -5,7 +5,12 @@ export default (state = 0, action) => {
 
     switch ( type ){
         case INCREMENT: return state + 1
-        case DECREMENT: return state - 1
+        case DECREMENT: 
+            if(state === 0){
+                return 0
+            }else{
+                return state - 1
+            }
         case RESET    : return state = 0
     }
 
