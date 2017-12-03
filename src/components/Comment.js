f2625d04ee4fac18712f2ef5acebffa74908f967
Muslim Guseinov <gusejnov1999@list.ru>
2017-12-03 22:38:15 +0300

    MapStateToProps for the reselection of comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { commentSelectorFactory } from '../selectors'
 
-export default function Comment({comment}) {
+function Comment({comment}) {
     return (
         <div className="article-comment__info">
             <p>{comment.text} </p><b className="article-comment__info_user">by {comment.user}</b>
@@ -10,8 +12,22 @@ export default function Comment({comment}) {
 }
 
 Comment.propTypes = {
+    id: PropTypes.string.isRequired,
+    //from connect
     comment: PropTypes.shape({
         text: PropTypes.string.isRequired,
         user: PropTypes.string.isRequired
     }).isRequired
 }
+
+const mapStateToProps = () => {
+    const commentSelector = commentSelectorFactory()
+
+    return (state, ownProps) => {
+        return {
+            comment: commentSelector(state, ownProps)
+        }
+    }
+}
+
+export default connect(mapStateToProps)(Comment)
