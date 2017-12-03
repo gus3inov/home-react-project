51972d9f20a6e6062aa8a40b667449b2d20a11fd
Muslim Guseinov <gusejnov1999@list.ru>
2017-12-03 22:40:38 +0300

    add middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducer from '../reducer'
+import logger from '../middlewares/logger'
+
+const enhancer = applyMiddleware(logger)
 
 const store = createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), enhancer)
 
 /**
  * @default dev only
