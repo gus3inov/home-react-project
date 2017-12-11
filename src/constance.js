3c448728afe1fe224847dc8df61690606a0be3b8
Muslim Guseinov <gusejnov1999@list.ru>
2017-12-11 16:29:35 +0300

    Константа для добавление комментариев

--- src/constance.js
+++ src/constance.js
@@ -1,7 +1,11 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 export const RESET = 'RESET'
+
 export const DELETE_ARTICLE = 'DELETE_ARTICLE'
+
 export const INPUT_ARTICLE = 'INPUT_ARTICLE'
 export const SELECT_ARTICLE = 'SELECT_ARTICLE'
 export const DATE_ARTICLE = 'DATE_ARTICLE'
+
+export const ADD_COMMENT = 'ADD_COMMENT'
