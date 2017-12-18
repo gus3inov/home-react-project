6f7e33d0031b61e0d96742e9e7f433cfa7cc8e5a
Muslim Guseinov <gusejnov1999@list.ru>
2017-12-18 23:10:45 +0300

    прокидываем миддлвару в стор

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from 'redux'
 import reducer from '../reducer'
 import logger from '../middlewares/logger'
 import randomId from '../middlewares/randomId'
+import api from '../middlewares/api'
 
-const enhancer = applyMiddleware(randomId)
+const enhancer = applyMiddleware(randomId, api)
 
 const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), enhancer)
