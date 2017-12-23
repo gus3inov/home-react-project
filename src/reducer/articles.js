89bc1f8fc2a8575f31c67ce90ed63fc6f766bac8
gus3inov <gusejnov1999@list.com>
2017-12-23 19:52:54 +0300

    Добавляем immutable

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,30 +1,28 @@
 import { normalizedArticles as defaultArticles } from '../fixtures'
 import { DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES } from '../constance'
 import { arrToMap } from '../helpers'
+import {Map, Record} from 'immutable'
 
-const articleMap = arrToMap(defaultArticles)
+const defaultState = new Map({})
+const articleRecord = Record({
+    text: undefined,
+    title: 'Some Blog',
+    id: undefined,
+    comments: []
+})
 
-export default (articlesState = {  }, action) => {
+export default (articlesState = defaultState, action) => {
     const { type, payload, response, randomId } = action
 
     switch(type){
         case DELETE_ARTICLE:
-            const tmpState = articlesState
-            delete tmpState[payload.id]
-            return tmpState
+            articlesState.delete(payload.id)
 
         case ADD_COMMENT:
-            const article = articlesState[payload.articleId]
-            return {
-                ...articlesState,
-                [payload.articleId]: {
-                    ...article,
-                    comments: (article.comments || []).concat(randomId)
-                }
-            }
-            
+            return articlesState.updateIn([payload.articleId, 'comments'], comments =>  comments.concat(randomId))
+
         case LOAD_ALL_ARTICLES:
-            return arrToMap(response)
+            return arrToMap(response, articleRecord)
     }
 
     return articlesState
