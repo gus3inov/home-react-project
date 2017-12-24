718b7e3ca53f2e3f9129aa2e5ac449c0574cfe08
Muslim Guseinov <gusejnov1999@list.ru>
2017-12-24 23:32:34 +0300

    Добавление иммутабельности через immutable

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,28 +1,47 @@
 import { normalizedArticles as defaultArticles } from '../fixtures'
-import { DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES } from '../constance'
+import { DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES, START, SUCCESS } from '../constance'
 import { arrToMap } from '../helpers'
-import {Map, Record} from 'immutable'
+import {Map, Record, OrderedMap} from 'immutable'
 
-const defaultState = new Map({})
 const articleRecord = Record({
     text: undefined,
-    title: 'Some Blog',
+    title: '',
     id: undefined,
     comments: []
 })
+
+const ReducerState = Record({
+    loading: false,
+    loaded: false,
+    entities: new OrderedMap({})
+})
+
+const defaultState = new ReducerState()
 
 export default (articlesState = defaultState, action) => {
     const { type, payload, response, randomId } = action
 
     switch(type){
         case DELETE_ARTICLE:
-            articlesState.delete(payload.id)
+           return articlesState.deleteIn(['entities', payload.id])
 
         case ADD_COMMENT:
-            return articlesState.updateIn([payload.articleId, 'comments'], comments =>  comments.concat(randomId))
+            return articlesState.updateIn(
+                [
+                    'entities',
+                     payload.articleId, 
+                     'comments'
+                ], 
+                     comments =>  comments.concat(randomId))
 
-        case LOAD_ALL_ARTICLES:
-            return arrToMap(response, articleRecord)
+        case LOAD_ALL_ARTICLES + START:
+                return articlesState.set('loading', true)
+
+        case LOAD_ALL_ARTICLES + SUCCESS:
+            return articlesState
+                .set('entities', arrToMap(response, articleRecord))
+                .set('loading', false)
+                .set('loaded', true)
     }
 
     return articlesState
