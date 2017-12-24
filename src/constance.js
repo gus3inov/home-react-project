c775f9e8ad410c8b1056624b63cbe00ffdbae6b5
Muslim Guseinov <gusejnov1999@list.ru>
2017-12-24 23:18:49 +0300

    Константы для подгрузки по рест АПИ

--- src/constance.js
+++ src/constance.js
@@ -1,13 +1,18 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
-export const RESET = 'RESET'
+export const RESET     = 'RESET'
 
 export const DELETE_ARTICLE = 'DELETE_ARTICLE'
 
-export const INPUT_ARTICLE = 'INPUT_ARTICLE'
+export const INPUT_ARTICLE  = 'INPUT_ARTICLE'
 export const SELECT_ARTICLE = 'SELECT_ARTICLE'
-export const DATE_ARTICLE = 'DATE_ARTICLE'
+export const DATE_ARTICLE   = 'DATE_ARTICLE'
 
-export const ADD_COMMENT = 'ADD_COMMENT'
+export const ADD_COMMENT    = 'ADD_COMMENT'
 
 export const LOAD_ALL_ARTICLES = 'LOAD_ALL_ARTICLES'
+export const LOAD_ARTICLE = 'LOAD_ARTICLE'
+
+export const START   = '_START'
+export const SUCCESS = '_SUCCESS'
+export const FAIL    = '_FAIL'
