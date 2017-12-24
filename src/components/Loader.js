81096e6136d72a6eb6a059d80feb2e55f45e0f61
Muslim Guseinov <gusejnov1999@list.ru>
2017-12-24 23:20:46 +0300

    Создание функционального компонента Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Loader = () => {
+  return (
+    <div>
+        <h2>Loading...</h2>
+    </div>
+  )
+}
+
+Loader.propTypes = {
+
+}
+
+export default Loader
