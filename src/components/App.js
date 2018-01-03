f34bced9fe34077f650c5503a482a334d454e8fd
Muslim Guseinov <gusejnov1999@list.ru>
2018-01-03 19:27:10 +0300

    rounting по компонентам

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,37 @@ import MySelect from './Filters/MySelect'
 import ArticleList from './ArticleList'
 import MyDatePicker from './Filters/MyDatePicker'
 import Counter from './Counter'
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom'
 
 
 export default class App extends Component {
     
   render() {
     return (
-      <div>
-        <Counter />
+      <Router>
+        <div>
+          <div>
+            <h2>Main Menu</h2>
+              <nav>
+                  <ul>
+                    <li><NavLink activeStyle = {{ color: 'red' }} to = "/counter">Counter</NavLink></li>
+                    <li><NavLink activeStyle = {{ color: 'red' }} to = "/select">Select Filter</NavLink></li>
+                    <li><NavLink activeStyle = {{ color: 'red' }} to = "/date-picker">Date Picker Filter</NavLink></li>
+                    <li><NavLink activeStyle = {{ color: 'red' }} to = "/articles">Articles</NavLink></li>
+                    </ul>
+              </nav>
+          </div>
         <MyInput />
-        <MySelect />
-        <div className = "main">
-        <div className= "right-bar">
-        <MyDatePicker />
+          <Route path = "/counter" component = { Counter }/>
+          <Route path = "/select" component = { MySelect }/>
+          <div className = "main">
+            <div className= "right-bar">
+            <Route path = "/date-picker" component = { MyDatePicker }/>
+            </div>
+              <Route path = "/articles" component = { ArticleList }/>
+          </div>
         </div>
-        <ArticleList />
-      </div>
-      </div>
+      </Router>
     )
   }
 
