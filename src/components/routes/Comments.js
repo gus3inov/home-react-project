f2f208d054b0a8caf334043cf474103ac9f9a13f
Muslim Guseinov <gusejnov1999@list.ru>
2018-01-04 21:30:34 +0300

    Компонента для получение комментарев по роутингу

---
+++ src/components/routes/Comments.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Route, Link } from 'react-router-dom'
+
+export class componentName extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default componentName
