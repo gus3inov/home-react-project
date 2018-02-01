b0e1ee18cfd6f0326017e2613641417f51527ba1
Muslim Guseinov <gusejnov1999@list.ru>
2018-02-01 23:30:45 +0300

    Добавлена стилизация компонентов главной страницы

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,32 @@
 import React, {PureComponent} from 'react'
 import PropTypes from 'prop-types'
+import { Title } from '../styled-components'
+import styled from 'styled-components'
+
+const Span = styled.span`
+        background: ${props => props.primary ? 'palevioletred' : 'white'};
+        padding: 0.25em 1em;
+    `
 
 export default class MainPage extends PureComponent {
     static propTypes = {}
 
+    state = {
+        background: true
+    }
+
     render() {
+        console.log(this.state.background)
         return (
             <div>
-                <h3>Главная</h3>
+                <Title> Главная </Title>
+
+        <Span primary = {this.state.background}>Что то</Span>
+                <button onClick={() => {
+                    this.setState({
+                        background: !this.state.background
+                    })
+                }}>Кнопка</button>
             </div>
         )
     }
